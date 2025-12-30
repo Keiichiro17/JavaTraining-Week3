399173da152eb718a225fc2ebf43223c477db226
Mike <244047558+Keiichiro17@users.noreply.github.com>
2025-12-30 13:19:46 +0900

    Add Task class with constructor and fields

--- src/Task.java
+++ src/Task.java
@@ -1,1 +1,11 @@
+class Task{
+  String title;
+  String completed state;
+  int deadline;
 
+  Task(String title, String completed state, int deadline){
+this.title=title;
+this.completed state =completed state ;
+this.deadline=deadline;
+  }
+}
