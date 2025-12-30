d8cb5309e8504d58a54cc0b6bd44474db9dde449
Mike <244047558+Keiichiro17@users.noreply.github.com>
2025-12-31 00:55:01 +0900

    Update Task.java

--- src/Task.java
+++ src/Task.java
@@ -1,15 +1,24 @@
+//日付を扱うためのクラス
 import java.time.LocalDate;
 
 class Task{
+  //タスクのタイトル
   String title;
+  //タスクが完了しているか
   boolean completed;
+  //タスクの締切日
   LocalDate deadline;
 
   Task(String title, boolean completed, LocalDate deadline){
 this.title=title;
+//完了状態をセット
 this.completed=completed;
+//締切日をセット
 this.deadline=deadline;
   }
+  //完了状態かどうかを文字列で返す
+  //完了していれば「[✓完了]」
+  //未完了なら「表示なし]
 String statusLabel(){
   return completed ? "[✓完了]":"";
     }
