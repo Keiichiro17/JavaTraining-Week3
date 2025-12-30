9c3c42f2bb6cc28cec8a51cdcb3469247ab890c9
Mike <244047558+Keiichiro17@users.noreply.github.com>
2025-12-31 01:22:43 +0900

    Update TaskManager.java

--- src/TaskManager.java
+++ src/TaskManager.java
@@ -1,13 +1,17 @@
 import java.util.ArrayList;
 import java.util.concurrent.atomic.AtomicInteger;
 
+//タスク全体を管理するクラス
+//追加・一覧表示・削除・完了処理をする
 class TaskManager{
-  ArrayList<Task> tasks = new ArrayList<>();
+  ArrayList<Task> tasks = new ArrayList<>();//Taskを複数保存する
 
+  //タスクを追加
   void addTask(Task task){
     tasks.add(task);
   }
 
+  //登録されているタスクを一覧表示
   void showTasks(){
     if(tasks.size() == 0){
       System.out.println("現在登録されているTaskはありません。");
@@ -19,6 +23,7 @@ class TaskManager{
     }
   }
 
+  //指定された番号のタスクを削除
   boolean removeTask(int number){
     if(number < 1 || number > tasks.size()){
       return false;
@@ -28,6 +33,7 @@ class TaskManager{
     return true;
   }
 
+  //指定された番号のタスクを完了にする
   boolean completeTask(int number){
     if(number < 1 || number > tasks.size()){
       return false;
@@ -37,6 +43,7 @@ class TaskManager{
     return true;
   }
 
+  //現在登録されているタスクを返す
   int size(){
     return tasks.size();
   }
