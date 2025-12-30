9aa5dcfdf3b68d445d17dacf0a6ec9269627abfb
Mike <244047558+Keiichiro17@users.noreply.github.com>
2025-12-30 13:30:06 +0900

    Implement TaskManager methods and fix bugs
    Fix errors in showTasks and removetasks methods, and update task list handling.

--- src/TaskManager.java
+++ src/TaskManager.java
@@ -1,1 +1,31 @@
+import java.util.ArrayList;
 
+class TaskManager{
+  ArrayList <Task> Tasks = new ArrayList<>();
+  
+  void addTask(Task task){
+    Tasks.add(task);
+  }
+  void showTasks(){
+    if(books.size()==0){
+      System.out.println("現在登録されているTaskはありません。");
+        }else{
+      for(int i=0;i<books.size();i++){
+        Task t=tasks.get(i);
+        System.out.println((i+1)+":"+t.title+"/"+t.completed state+"/"+t.deadline);
+      }
+    }
+  }
+  boolean removetasks(int number){
+    int index=number-1;
+    
+    if(number<1||number>tasks.size()){
+    return false;
+  }
+  tasks.remove(index);
+  return true;
+  }
+  int size(){
+    return tasks.size();
+  }
+  }
