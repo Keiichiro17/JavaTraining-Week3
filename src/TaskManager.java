926295ddde7251658c92fb6f3beefee45f68f909
Mike <244047558+Keiichiro17@users.noreply.github.com>
2025-12-30 15:36:47 +0900

    Update TaskManager.java

--- src/TaskManager.java
+++ src/TaskManager.java
@@ -1,29 +1,41 @@
 import java.util.ArrayList;
 
 class TaskManager{
-  ArrayList <Task> tasks = new ArrayList<>();
-  
+  ArrayList<Task> tasks = new ArrayList<>();
+
   void addTask(Task task){
     tasks.add(task);
   }
+
   void showTasks(){
     if(tasks.size()==0){
       System.out.println("現在登録されているTaskはありません。");
-        }else{
+    }else{
       for(int i=0;i<tasks.size();i++){
-        Task t=tasks.get(i);
-        System.out.println((i+1)+":"+t.title+"/"+t.completed+"/"+t.deadline);
+        Task t = tasks.get(i);
+        System.out.println((i+1)+":"+t.title+"/"+ t.statusLabel() +"/"+t.deadline);
       }
     }
   }
+
   boolean removeTask(int number){
-    int index=number-1;
-    if(number<1||number>tasks.size()){
-    return false;
-  }
- tasks.remove(index);
+    int index = number - 1;
+    if(number < 1 || number > tasks.size()){
+      return false;
+    }
+    tasks.remove(index);
     return true;
   }
+
+  boolean completeTask(int number){
+    int index = number - 1;
+    if(number < 1 || number > tasks.size()){
+      return false;
+    }
+    tasks.get(index).completed = true;
+    return true;
+  }
+
   int size(){
     return tasks.size();
   }
