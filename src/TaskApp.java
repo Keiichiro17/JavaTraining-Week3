09ad1978b3564f78264e409df62978a32bc79b6c
Mike <244047558+Keiichiro17@users.noreply.github.com>
2025-12-30 13:38:50 +0900

    Implement basic library management system

--- src/TaskApp.java
+++ src/TaskApp.java
@@ -1,1 +1,57 @@
+import java.util.Scanner;
+class LibraryApp{
+  public static void main(String[]args)
+  {
+    Scanner sc=new
+      Scanner(System.in);
+    TaskManager taskmanager=new taskmanager();
+
+    while(true){
+      System.out.println("図書管理システム");
+      System.out.println("1:書籍を登録する");
+      System.out.println("2:登録済みの書籍を一覧表示する");
+      System.out.println("3:書籍を削除する");
+      System.out.println("0:終了");
+
+      int choice=sc.nextInt();
+      sc.nextLine();
+
+      if(choice==1){
+        System.out.print("タイトル: ");
+        String title=sc.nextLine();
+
+        System.out.print("著者: ");
+        String author=sc.nextLine();
+
+        System.out.print("出版年: ");
+        int year=sc.nextInt();
+        sc.nextLine();
+
+        Book book=new Book(title,author,year);
+        library.addBook(book);
+        System.out.println("登録しました。");
+        
+      }else if (choice==2){
+      library.showBooks();
+        
+      }else if(choice==3){
+        System.out.print("削除する番号: ");
+        int num =sc.nextInt();
+        sc.nextLine();
+
+        if(!library.removeBook(num)){
+          System.out.println("削除できませんでした");
+        }
+
+      }else if (choice==0){
+        System.out.println("終了");
+        break;
+        
+      }else{
+      System.out.println("0~3を入力してください");
+      }
+    }
+    sc.close();
+  }
+}
 
